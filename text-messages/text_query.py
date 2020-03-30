c9d74a509aded353cf8830812870d7bf625541b8
dhruvpillai6 <dhruvpillai6@gmail.com>
2020-03-30 00:08:14 -0400

    Handle dates--automatically dump data in the AllTexts class if there is more data than in the Conversation, and vice versa, to output prettier graphs.

--- text-messages/text_query.py
+++ text-messages/text_query.py
@@ -6,10 +6,13 @@ from pathlib import Path
 pandas.set_option('display.max_columns', None)
 pandas.options.mode.chained_assignment = None
 
-def text_query(WHERE='', LIMIT=''):
+def connect_to_iMessage_database():
     user = os.path.expanduser('~')
     path_to_database = Path(f'{user}/Library/Messages/chat.db')
-    conn = sqlite3.connect(path_to_database)
+    return sqlite3.connect(path_to_database)
+
+def text_query(WHERE='', LIMIT=''):
+    conn = connect_to_iMessage_database()
     SELECT_string = 'SELECT ' + \
                     'datetime(message.date/1000000000 + strftime("%s", "2001-01-01"),' \
                     '"unixepoch", "localtime") AS local_date, handle.id, ' \
@@ -20,7 +23,7 @@ def text_query(WHERE='', LIMIT=''):
                         'handle ON message.handle_id = handle.ROWID '
     ORDER_BY_string = 'ORDER BY ' + \
                       'date DESC '
-    if WHERE == '':
+    if WHERE == ('' or None):
         WHERE_string = ''
     else:
         WHERE_string = 'WHERE ' + \
@@ -35,5 +38,12 @@ def text_query(WHERE='', LIMIT=''):
 
     return pandas.read_sql_query(SQL_STRING, conn)
 
+def generate_address_book():
+    conn = connect_to_iMessage_database()
+    dataframe = pandas.read_sql_query('SELECT ROWID, id FROM handle', conn)
+    dataframe = dataframe.rename(columns={'ROWID': 'handle_id'})
+    return dataframe.set_index('handle_id')
+
 if __name__ == '__main__':
-    print(type(text_query(WHERE='handle_id = 1', LIMIT=10).loc[0, 'local_date']))
+    # print(type(text_query(WHERE='handle_id = 1', LIMIT=10).loc[0, 'local_date']))
+    print(generate_address_book())
